5fc652b4f601acef83ded062f4fc57886f550761
Ian @ epicodus <ian@iancmacdonald.com>
2015-07-28 13:43:47 -0700

    Complete through exercise 3

---
+++ program.js
@@ -0,0 +1,17 @@
+fs   = require('fs')
+path = require('path')
+
+// Exercise 4
+
+
+// Exercise 3
+// process.stdin.pipe(process.stdout)
+
+// Exercise 2
+// file = path.resolve(process.argv[2])
+//
+// input = fs.createReadStream(file)
+// input.pipe(process.stdout)
+
+// Exercise 1
+// console.log('beep boop');
