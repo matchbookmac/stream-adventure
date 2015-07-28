2b8a4dcec9d001e1ad6f0a2493dfe90e9aa78654
Ian @ epicodus <ian@iancmacdonald.com>
2015-07-28 14:29:25 -0700

    Complete #6

--- program.js
+++ program.js
@@ -1,28 +1,45 @@
-var fs      = require('fs')
-var path    = require('path')
-var through = require('through2')
-var split   = require('split')
-
-// Exercise 5
 var
-  stream = through(write),
-  count = 1
+  fs      = require('fs'),
+  path    = require('path'),
+  through = require('through2'),
+  split   = require('split'),
+  concat  = require('concat-stream')
 ;
 
-function write(line, encoding, next) {
-  this.push('\n' + (((count % 2) === 0)
-    ? line.toString().toUpperCase()
-    : line.toString().toLowerCase()
-  ));
-  count++;
-  next();
+// Exercise 6
+function reverseStream(body) {
+  console.log(
+    body.toString()
+      .split('')
+      .reverse()
+      .join('')
+  );
 }
 
 process.stdin
-  .pipe(split())
-  .pipe(stream)
-  .pipe(process.stdout)
+  .pipe(concat(reverseStream))
 ;
+
+// Exercise 5
+// var
+//   stream = through(write),
+//   count = 1
+// ;
+//
+// function write(line, encoding, next) {
+//   this.push('\n' + (((count % 2) === 0)
+//     ? line.toString().toUpperCase()
+//     : line.toString().toLowerCase()
+//   ));
+//   count++;
+//   next();
+// }
+//
+// process.stdin
+//   .pipe(split())
+//   .pipe(stream)
+//   .pipe(process.stdout)
+// ;
 
 // Exercise 4
 // var stream = through(write, end);
