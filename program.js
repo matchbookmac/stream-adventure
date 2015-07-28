a6ee4a5d4219e1b3924d5816d3463cdce2cdee16
Ian @ epicodus <ian@iancmacdonald.com>
2015-07-28 14:16:53 -0700

    Complete #5

--- program.js
+++ program.js
@@ -1,20 +1,42 @@
-fs      = require('fs')
-path    = require('path')
-through = require('through2')
+var fs      = require('fs')
+var path    = require('path')
+var through = require('through2')
+var split   = require('split')
 
-// Exercise 4
-var stream = through(write, end);
+// Exercise 5
+var
+  stream = through(write),
+  count = 1
+;
 
-function write(buffer, encoding, next) {
-  this.push(buffer.toString().toUpperCase());
+function write(line, encoding, next) {
+  this.push('\n' + (((count % 2) === 0)
+    ? line.toString().toUpperCase()
+    : line.toString().toLowerCase()
+  ));
+  count++;
   next();
 }
 
-function end(done) {
-  done();
-}
+process.stdin
+  .pipe(split())
+  .pipe(stream)
+  .pipe(process.stdout)
+;
 
-process.stdin.pipe(stream).pipe(process.stdout);
+// Exercise 4
+// var stream = through(write, end);
+//
+// function write(buffer, encoding, next) {
+//   this.push(buffer.toString().toUpperCase());
+//   next();
+// }
+//
+// function end(done) {
+//   done();
+// }
+//
+// process.stdin.pipe(stream).pipe(process.stdout);
 
 // Exercise 3
 // process.stdin.pipe(process.stdout)
